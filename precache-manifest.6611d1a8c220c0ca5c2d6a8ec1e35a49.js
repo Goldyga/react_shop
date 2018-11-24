7004e2cd9905ff5d2b7d7219359b0f0174eec216
Marcin Goldyga <marcingo@toya.net.pl>
2018-11-24 16:29:21 +0100

    Updates

---
+++ precache-manifest.6611d1a8c220c0ca5c2d6a8ec1e35a49.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react_shop/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "248b2f67c36dd954492c",
+    "url": "/react_shop/static/js/runtime~main.248b2f67.js"
+  },
+  {
+    "revision": "db019b1754b8f06fb032",
+    "url": "/react_shop/static/js/main.db019b17.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/react_shop/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "db019b1754b8f06fb032",
+    "url": "/react_shop/static/css/main.c514d258.chunk.css"
+  },
+  {
+    "revision": "4a70ee27c8ebc63967fff82bad1c4a81",
+    "url": "/react_shop/index.html"
+  }
+];
